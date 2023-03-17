95db12d7c0c7cc104bc180b2137d200cc9549b23
ashishh-tomar <ashishtomar692@gmail.com>
2023-03-17 22:27:09 +0530

    Added responsiveness and improved movies

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Banner() {
   return (
     <>
-      <div className="bg-banner h-[80vh] bg-center bg-cover flex items-end">
-        <div className="text-xl text-white bg-gray-900 bg-opacity-50 text-center p-3 w-full">
+      <div className="bg-banner h-[80vh] md:h-[90vh] bg-center bg-cover flex items-end">
+        <div className="text-xl md:text-3xl text-white bg-gray-900 bg-opacity-50 text-center p-3 w-full">
           Avatar 2 : The way of Water
         </div>
       </div>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,13 +5,33 @@ function Movies() {
   return (
     <div className="mt-8">
       <div className="mb-8 font-bold text-2xl text-center">Trending Movies</div>
+
       <div className="flex flex-wrap justify-center">
-        <img src={image} className="w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300" alt="" />
-        <img src={image} className="w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300" alt="" />
-        <img src={image} className="w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300" alt="" />
-        <img src={image} className="w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300" alt="" />
-        <img src={image} className="w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300" alt="" />
-        <img src={image} className="w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300" alt="" />
+        <div className="bg-[url(http://localhost:3000/static/media/bannerImg.eace6604962719e1bcf9.jpg)] bg-center bg-cover w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300 border-4 flex items-end">
+          <div className="font-bold text-white bg-gray-900 bg-opacity-50 text-center p-3 w-full rounded-b-xl">
+            Avatar
+          </div>
+        </div>
+
+        <div className="bg-[url(http://localhost:3000/static/media/bannerImg.eace6604962719e1bcf9.jpg)] bg-center bg-cover w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300 border-4 flex items-end">
+          <div className="font-bold text-white bg-gray-900 bg-opacity-50 text-center p-3 w-full rounded-b-xl">
+            Avatar
+          </div>
+        </div>
+
+        <div className="bg-[url(http://localhost:3000/static/media/bannerImg.eace6604962719e1bcf9.jpg)] bg-center bg-cover w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300 border-4 flex items-end">
+          <div className="font-bold text-white bg-gray-900 bg-opacity-50 text-center p-3 w-full rounded-b-xl">
+            Avatar
+          </div>
+        </div>
+
+        <div className="bg-[url(http://localhost:3000/static/media/bannerImg.eace6604962719e1bcf9.jpg)] bg-center bg-cover w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300 border-4 flex items-end">
+          <div className="font-bold text-white bg-gray-900 bg-opacity-50 text-center p-3 w-full rounded-b-xl">
+            Avatar
+          </div>
+        </div>
+
+        {/* <img src={image} className="w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300" alt="" /> */}
       </div>
     </div>
   );
