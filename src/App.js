6455441ec630bb847b00248cc1949c6da245e725
ashishh-tomar <ashishtomar692@gmail.com>
2023-03-17 22:59:52 +0530

    Routing in React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Navbar from './components/Navbar';
-import Banner from './components/Banner';
-import Movies from './components/Movies';
-import Pagination from './components/Pagination';
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Movies from "./components/Movies";
+import Favourites from "./components/Favourites";
+import Pagination from "./components/Pagination";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
-      <React.Fragment>
+    <React.Fragment>
+      <BrowserRouter>
         <Navbar></Navbar>
-        <Banner></Banner>
-        <Movies></Movies>
-        <Pagination></Pagination>
-      </React.Fragment>
-      
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner></Banner>
+                <Movies></Movies>
+                <Pagination></Pagination>
+              </>
+            }
+          ></Route>
+          <Route
+            path="/favourites"
+            exact
+            element={<Favourites></Favourites>}
+          ></Route>
+          <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <h1 className='text-3xl text-center mt-7'>PAGE NOT FOUND !!!</h1>
+  )
+}
+
+export default PageNotFound
