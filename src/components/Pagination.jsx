76b01cc0df330fb4f8e6e2d57e8ced9353de1fda
ashishh-tomar <ashishtomar692@gmail.com>
2023-03-18 16:02:13 +0530

    Dynamic Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Pagination(){
+
+    let [pageNum,setPage] = useState(1);
+
+    let onPrev = () =>{
+        if(pageNum > 1)
+        {
+            setPage(pageNum-1);
+        }
+    }
+
+    let onNext = () =>{
+        
+            setPage(pageNum+1);
+    }
+
     return(
         <div className="flex justify-center my-4">
-            <div className="border-2 p-2 border-r-0 rounded-l-xl">Prev</div>
-            <div className="border-2 p-2 ">1</div>
-            <div className="border-2 p-2 border-l-0 rounded-r-xl">Next</div>
+            <div className="border-2 p-2 border-r-0 rounded-l-xl cursor-pointer" onClick={onPrev}>Prev</div>
+            <div className="border-2 p-2 ">{pageNum}</div>
+            <div className="border-2 p-2 border-l-0 rounded-r-xl cursor-pointer" onClick={onNext}>Next</div>
         </div>
     )
 }
