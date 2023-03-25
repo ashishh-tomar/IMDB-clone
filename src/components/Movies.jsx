09353a1939cbb910c4ebf27d9e4326999ef985b4
ashishh-tomar <ashishtomar692@gmail.com>
2023-03-25 16:24:17 +0530

    Emoji functionality added so that movie can be added to favourites or not

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -9,23 +9,9 @@ import Pagination from "./Pagination"
 
 function Movies() {
   let [movies, setMovies] = useState([]);
-
   let [pageNum,setPage] = useState(1);
-
-  let onPrev = () =>{
-      if(pageNum > 1)
-      {
-          setPage(pageNum-1);
-      }
-  }
-
-  let onNext = () =>{
-      
-          setPage(pageNum+1);
-  }
-
-
-
+  let [hovered,setHovered] = useState("");
+  let [favourites,setFavourites] = useState([]);
 
 
 
@@ -42,6 +28,69 @@ function Movies() {
         });
     })();
   }, [pageNum]);
+
+
+
+  // Pagination Handlers
+  let onPrev = () =>{
+      if(pageNum > 1)
+      {
+          setPage(pageNum-1);
+      }
+  }
+
+  let onNext = () =>{
+      
+          setPage(pageNum+1);
+  }
+
+
+
+
+  // Emoji Handlers on Hover
+  let showEmoji = (id)=>{
+    setHovered(id);
+  }
+
+  let hideEmoji = ()=>{
+    setHovered("");
+  }
+
+
+
+  // Adding movie id to favourites so the emoji shown as liked or disliked accodingly
+  let addToFavourites = (id)=>{
+    console.log("Liked");
+    let newFav = [...favourites,id];
+    setFavourites(newFav);
+    console.log(favourites);
+    
+  }
+
+  let removeFromFavourites = (id)=>{
+
+    console.log("DisLiked");
+    let filteredArr = favourites.filter((ele)=>{
+      return ele !== id;
+    })
+
+    setFavourites(filteredArr);
+    console.log(favourites);
+    
+  }
+
+
+
+
+
+  // Making API Request to fetch data
+  
+
+
+
+
+
+
 
   return (
     <div className="mt-8">
@@ -61,15 +110,26 @@ function Movies() {
           </div>
         ) : (
           movies.map((movie) => {
-            console.log(movie);
+            //console.log(movie);
             return (
               <div
                 key={movie.id}
+                onMouseOver={()=>{showEmoji(movie.id);}}
+                onMouseLeave={()=>{hideEmoji()}}
                 className=" bg-center bg-cover w-[175px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300 border-4 flex items-end"
                 style={{
                   backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.poster_path})`,
                 }}
               >
+
+               
+                <div className="p-2  bg-gray-900 bg-opacity-50 rounded-xl absolute top-2 right-2" style={{display : hovered == movie.id ? "block" : "none"}}>
+                  {
+                    favourites.includes(movie.id) == true ?  <div className="text-2xl cursor-pointer" onClick={()=>{removeFromFavourites(movie.id)}}>❌</div> : <div className="text-2xl cursor-pointer" onClick={()=>{addToFavourites(movie.id)}}>❤️</div>
+                  }
+                  
+                </div>
+
                 <div className="font-bold text-white bg-gray-900 bg-opacity-50 text-center p-3 w-full rounded-b-xl">
                   {movie.name == null ? movie.title : movie.name}
                 </div>
