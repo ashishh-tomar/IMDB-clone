7767620703fc492887d27d04fa75bf3c7b3b86c5
ashishh-tomar <ashishtomar692@gmail.com>
2023-03-28 23:07:23 +0530

    Added static UI in Favourites Page

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Pagination from "./Pagination"
 
 function Favourites() {
   return (
-    <div>Favourites</div>
+    <>
+    <div className='mt-6 flex space-x-2 justify-center'>
+      <button className='py-1 px-2 bg-yellow-500 rounded-lg text-lg font-bold '>All Genre</button>
+      <button className='py-1 px-2 bg-gray-300 rounded-lg text-lg font-bold hover:bg-yellow-500 '>All Genre</button>
+    </div>
+
+    <div className='mt-6 flex justify-center space-x-3'>
+      <input type="text" className='border-2 py-1 px-2 text-center' placeholder='Search'></input>
+      <input type="number" className='border-2 py-1 px-2 text-center' value={1}></input>
+    </div>
+    
+
+    <Pagination></Pagination>
+    </>
   )
 }
 
